bd9d59d94b6ae099c7c66de2a45b1c39b982cd5d
fakoeste <Ace@83ad377e.funky.hs-osnabrueck.de>
2015-05-21 14:01:51 +0200

    Architektur
    change Architektur

---
+++ src/main/java/de/hsos/im/iwash/ui/Controller/DashboardUIController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsos.im.iwash.ui.Controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Ace
+ */
+@Named(value = "dashboardUIController")
+@Dependent
+public class DashboardUIController {
+
+    /**
+     * Creates a new instance of dashboardUIController
+     */
+    public DashboardUIController() {
+    }
+    
+}
