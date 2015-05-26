c4de749c1a2d12a4c0ca994811562b3ddf531570
Holger Tenbeitel <tenbeitelholger@gmx.de>
2015-05-26 14:16:18 +0200

    slick

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -7,6 +7,12 @@
 
 $(document).ready(function () {
     $('.single-item').slick({
-        setting - name: setting - value
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
 });
